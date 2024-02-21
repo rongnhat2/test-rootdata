842f59e8fcedf520fb51e6f236c3d825b2f5c58f
Nguyen Nhat Long <long.nguyen2198@gmail.com>
2024-02-21 09:47:01 +0700

    update base

--- run2.py
+++ run2.py
@@ -38,9 +38,10 @@ while True:
 		for request in driver.requests:
 			if request.method == 'POST' and "org_detail" in request.url:
 				payload_data = request.body.decode('UTF-8')
-				url = "https://launch.rhass.vn/api/itemInvestor?id"+str(x["id"])+"&payload="+str(x["payload_data"])
+				url = "https://launch.rhass.vn/api/itemInvestor?id"+str(x["id"])+"&payload="+str(payload_data)
 				requests.get(url)
 	except Exception as e:
+		print(e)
 		url = "https://launch.rhass.vn/api/itemProject?id"+str(x["id"])
 		requests.get(url)
 		print("* Project Link *")
